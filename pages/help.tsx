18e39ccd98ad47e2e55ef8f809a3e898e1079c53
MalitsPlus <hua8102259@gmail.com>
2023-01-21 23:19:20 +0900

    feat: update help

--- pages/help.tsx
+++ pages/help.tsx
@@ -1,5 +1,6 @@
 import { logEvent } from "firebase/analytics";
 import Head from "next/head";
+import Link from "next/link";
 import { useEffect } from "react";
 import { analytics } from "../src/firebase/firebase";
 
@@ -12,9 +13,18 @@ export default function Help() {
       <Head>
         <title>Hoshimi Stage - Help</title>
       </Head>
-      <div>
-        this is about page.
-      </div>
+      <p>
+        Under construction.
+      </p>
+      <p>
+        Open source homepage: <Link href="https://github.com/MalitsPlus/HoshimiStage">https://github.com/MalitsPlus/HoshimiStage</Link>
+      </p>
+      <p>
+        License: Apache-2.0 license
+      </p>
+      <p>
+        {"Disclaimer: This is a fan-made website and has no connection with the official affiliation. The copyright of ingame data & assets are belong to QualiArts, Inc."}
+      </p>
     </>
   )
 }
