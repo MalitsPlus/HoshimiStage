48a0561d5b77c2a5c2cfc1c214fddda30a7192e0
MalitsPlus <50281195+MalitsPlus@users.noreply.github.com>
2023-10-21 14:24:45 +0900

    feat: add CSP

--- next.config.js
+++ next.config.js
@@ -11,6 +11,27 @@ const nextConfig = {
         pathname: '/api/img/**',
       },
     ],
+    unoptimized: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              connect-src 'self' hoshimi-backend.vibbit.me *.googleapis.com *.google-analytics.com;
+              img-src 'self' blob: data: idoly-assets-curator.vercel.app res.cloudinary.com
+            `.replace(/\s{2,}/g, " ").trim(),
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "unsafe-none",
+          },
+        ],
+      },
+    ]
   },
 }
 
